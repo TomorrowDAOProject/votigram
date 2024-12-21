ab48c3e3c21555920445dfa37d0fcac1e1613984
Leon Zhang <lorenzo@tmrwdao.com>
2024-12-21 14:05:19 +1100

    feat: update buffer config

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./styles/theme.css";
 import "./index.css";
 import "./assets/fonts/votigram-icon.css";
+import { Buffer } from 'buffer';
 
 import App from "./App";
+
+window.Buffer = Buffer;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
