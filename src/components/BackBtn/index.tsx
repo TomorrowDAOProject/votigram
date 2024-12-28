3e97b9b017536f2fb178066592d8ea7cfa36b573
Leon Zhang <lorenzo@tmrwdao.com>
2024-12-28 19:49:37 +1100

    feat: update back buttom

--- src/components/BackBtn/index.tsx
+++ src/components/BackBtn/index.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const BackBtn: React.FC = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
   const handleGoBack = () => {
-    console.log('location.state', location.state)
-    if (location.state?.fromHome) {
-      navigate(-1);
+    if (location.state?.from) {
+      navigate(location.state?.from, { replace: true });
     } else {
-      navigate('/');
+      navigate(-1);
     }
   };
-
   return (
     <button
       onClick={handleGoBack}
       className="bg-transparent p-0 m-0 w-[24px] h-[24px] leading-[24px] focus:outline-none z-10"
     >
-      <i className='votigram-icon-back text-[24px] text-white' />
+      <i className="votigram-icon-back text-[24px] text-white" />
     </button>
   );
 };
