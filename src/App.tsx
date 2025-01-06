fac6dd230f9812b67b9978db00b9fae95691dd2b
evan-spidy <evan+1@tmrwdao.com>
2025-01-07 00:44:11 +0800

    Fix/side chain fix (#49)
    * Release/1.0.0: Votigram Launch (#48)
    
    * hotfix: remove config
    
    * hotfix: add condition for build
    
    * Chore/update workflow (#43)
    
    chore: update workflow file
    
    * Chore/update workflow (#44)
    
    chore: update workflow
    
    * Feat/tg bugfix (#45)
    
    * hotfix: remove config (#42)
    
    Co-authored-by: evan-spidy <evan@tmrwdao.com>
    
    * fix: fixed rewards points
    
    * feat: update home page
    
    * fix: fixed claim issue when request failed
    
    * feat: update signature of collect request
    
    * feat: update signature
    
    * feat: update collect params
    
    ---------
    
    Co-authored-by: evan-spidy <evan+1@tmrwdao.com>
    Co-authored-by: evan-spidy <evan@tmrwdao.com>
    
    * Feat/tg bugfix (#46)
    
    * hotfix: remove config (#42)
    
    Co-authored-by: evan-spidy <evan@tmrwdao.com>
    
    * fix: fixed rewards points
    
    * feat: update home page
    
    * fix: fixed claim issue when request failed
    
    * feat: update signature of collect request
    
    * feat: update signature
    
    * feat: update collect params
    
    * feat: update display name
    
    * feat: update user info
    
    ---------
    
    Co-authored-by: evan-spidy <evan+1@tmrwdao.com>
    Co-authored-by: evan-spidy <evan@tmrwdao.com>
    
    ---------
    
    Co-authored-by: evan-spidy <evan@tmrwdao.com>
    Co-authored-by: Leon-pix-coder <lorenzo@tmrwdao.com>
    
    * fix: add side chain in env
    
    ---------
    
    Co-authored-by: evan-spidy <evan@tmrwdao.com>
    Co-authored-by: Leon-pix-coder <lorenzo@tmrwdao.com>

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,11 @@ const App = () => {
     const htmlElement = document.getElementsByTagName("html")[0];
     if (window?.Telegram && isTMA("simple")) {
       if (
-        (window.Telegram.WebApp?.platform === "ios" ||
+        ((window.Telegram.WebApp?.platform === "ios" ||
           window.Telegram.WebApp?.platform === "android") &&
-        window.Telegram.WebApp?.version &&
-        Number(window.Telegram.WebApp?.version) >= 8
+          window.Telegram.WebApp?.version &&
+          Number(window.Telegram.WebApp?.version) >= 8) ||
+        window.Telegram.WebApp?.platform?.includes("web")
       ) {
         window.Telegram.WebApp?.requestFullscreen?.();
 
